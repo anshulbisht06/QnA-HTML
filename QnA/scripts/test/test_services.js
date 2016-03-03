6f7b8e6e7aa63ae726fc8eaef6fec085534cb859
Gaurav Tygai <grvtyagi22@gmail.com>
2016-03-03 14:26:01 +0530

    login data save success before test

--- QnA/scripts/test/test_services.js
+++ QnA/scripts/test/test_services.js
@@ -1,17 +1,27 @@
 angular.module('QnA')
 .constant('baseURL',baseURL)
 
-.service('testUserDataFactory', ['$http', 'baseURL', function($http, baseURL) {
-        this.createSubCategory = function(data){
-        		var config = {
-	                headers : {
-	                }
-	            };
-        		return $http.post(baseURL+'user/data/',data, config)
-		            .success(function(data, status, headers, config) {
-		          })
-		            .error(function logoutErrorFn(data, status, headers, config) {
-		            	console.error('Cannot logout!!!');
-		          })
+.service('testUserDataFactory', ['$resource', 'baseURL', function($resource, baseURL) {
+	this.createSubCategory = function(data){
+            return $resource(baseURL+"user/data/", null,
+                    {'update':   
+                    { method:'POST'/*, headers: {'Authorization': 'JWT ' + token}*/} 
+                    },
+                    { stripTrailingSlashes: false }
+                    );
         };
+        // this.createSubCategory = function(data){
+        // 		var config = {
+	       //          headers : {
+	       //          }
+	       //      };
+        // 		return $http.post(baseURL+'user/data/',data, config)
+		      //       .success(function(data, status, headers, config) {
+		      //       	return data;
+		      //     })
+		      //       .error(function logoutErrorFn(data, status, headers, config) {
+		      //       	console.error('Cannot logout!!!');
+		      //       	return data;
+		      //     })
+        // };
     }])
