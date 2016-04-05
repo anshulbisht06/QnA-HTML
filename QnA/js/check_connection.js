d263c1036c5e7b30557bcdfb26e7f181a2cb45b9
anshulbisht06 <anshul.bisht06@gmail.com>
2016-04-05 13:51:50 +0530

    pull from origin

--- QnA/js/check_connection.js
+++ QnA/js/check_connection.js
@@ -1,27 +1,24 @@
-// Requirments
-// 1. Jquery
-// Global var,functions ..
 var time = 10;
 var isTimerOn = false;
 // Call on Try Now button
 function ping_fn(call_count){
-          if(isTimerOn){
-            $.ajax({
-                  url : baseURL+"ping/",
-                  data : {},
-                  type : "GET",
-                  success : function(data) {
-                    $('#msg').text("Connected ...");
-                    // $('#connectionLostModal').modal('hide');
-                    time = -1;
-                    $('#connectionLostModal').modal('hide');
-                  },
-                  error : function(xhr,errmsg,err) {
-                    time = time + 5*call_count+1;
-                    // $('#time').text(time);
-                }
-                });
-          }
-            return false;
-        }
-// End
+  if(isTimerOn){
+    $.ajax({
+      url : baseURL+"ping/",
+      data : {},
+      type : "GET",
+      success : function(data) {
+        $('#msg').text("Connected ...");
+        // $('#connectionLostModal').modal('hide');
+        time = -1;
+        $('#connectionLostModal').modal('hide');
+        window.location.reload();
+      },
+      error : function(xhr,errmsg,err) {
+        time = time + 5*call_count+1;
+        // $('#time').text(time);
+    }
+    });
+  }
+    return false;
+}
