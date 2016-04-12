8fc5e1610cdee0a11e9fd05d1674f2701b019913
Ubuntu <ubuntu@ip-172-31-3-185.ap-southeast-1.compute.internal>
2016-04-12 08:53:05 +0000

    add server.js in .gitignore filw

--- server.js
+++
@@ -1,9 +0,0 @@
-var connect = require('connect'), serveStatic = require('serve-static');
-
-var app = connect();
-
-app.use(serveStatic("QnA"));
-app.listen(5001, function(){
-	console.log('TEST SERVER >>>>>>>> listening on 5001 port');
-    console.log(__dirname+'/QnA');
-});
