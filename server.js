fbaa425cb840071ada22e8dff877a16927b081ad
Gaurav Tygai <grvtyagi22@gmail.com>
2016-04-12 14:19:25 +0530

    add changes to server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var connect = require('connect'), serveStatic = require('serve-static');
 var app = connect();
 
 app.use(serveStatic("QnA"));
-app.listen(5000, function(){
-	console.log('listening on 5000 port');
+app.listen(5001, function(){
+	console.log('TEST SERVER >>>>>>>> listening on 5001 port');
     console.log(__dirname+'/QnA');
 });
