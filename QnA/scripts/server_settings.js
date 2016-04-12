ad17292f810d2941c6573a700469b78912bf19d8
Gaurav Tygai <grvtyagi22@gmail.com>
2016-04-12 11:54:34 +0530

    remove settings file

--- QnA/scripts/server_settings.js
+++
@@ -1,5 +0,0 @@
-// // All settings related to server setup
-// var baseURL= 'http://52.77.85.150/api/';
-// var serverURL= 'http://52.77.85.150/api/';
-// var appmodule = angular.module('QnA', ['ui.router', 'ngResource', 'ngCookies', 'ngFileUpload']);
-
